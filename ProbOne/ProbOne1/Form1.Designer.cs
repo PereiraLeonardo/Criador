59623ce37565f2ba561bc2cdfff583034c3ac60c
Leo <leo.rnp@outlook.com>
2021-10-25 11:22:56 -0300

    Versão: v.1.0.1

---
+++ ProbOne/ProbOne1/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace ProbOne1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ckb1 = new System.Windows.Forms.CheckBox();
+            this.ckb2 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.txb1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txb2 = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.listB1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txb3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ckb1
+            // 
+            this.ckb1.AutoSize = true;
+            this.ckb1.Location = new System.Drawing.Point(55, 55);
+            this.ckb1.Name = "ckb1";
+            this.ckb1.Size = new System.Drawing.Size(147, 17);
+            this.ckb1.TabIndex = 0;
+            this.ckb1.Text = "Número de Possibilidades";
+            this.ckb1.UseVisualStyleBackColor = true;
+            this.ckb1.CheckedChanged += new System.EventHandler(this.Ckb1_CheckedChanged);
+            // 
+            // ckb2
+            // 
+            this.ckb2.AutoSize = true;
+            this.ckb2.Location = new System.Drawing.Point(238, 55);
+            this.ckb2.Name = "ckb2";
+            this.ckb2.Size = new System.Drawing.Size(128, 17);
+            this.ckb2.TabIndex = 1;
+            this.ckb2.Text = "Valores em Conjuntos";
+            this.ckb2.UseVisualStyleBackColor = true;
+            this.ckb2.CheckedChanged += new System.EventHandler(this.Ckb2_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Número de Elementos";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(55, 415);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(669, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // txb1
+            // 
+            this.txb1.Location = new System.Drawing.Point(55, 164);
+            this.txb1.Name = "txb1";
+            this.txb1.Size = new System.Drawing.Size(100, 20);
+            this.txb1.TabIndex = 5;
+            this.txb1.TextChanged += new System.EventHandler(this.Txb1_TextChanged_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Formato do Agrupamento";
+            // 
+            // txb2
+            // 
+            this.txb2.Location = new System.Drawing.Point(55, 225);
+            this.txb2.Name = "txb2";
+            this.txb2.Size = new System.Drawing.Size(100, 20);
+            this.txb2.TabIndex = 7;
+            this.txb2.TextChanged += new System.EventHandler(this.Txb2_TextChanged_1);
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft Uighur", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Image = ((System.Drawing.Image)(resources.GetObject("btn1.Image")));
+            this.btn1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn1.Location = new System.Drawing.Point(238, 331);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(134, 51);
+            this.btn1.TabIndex = 10;
+            this.btn1.Text = "Arranjo!";
+            this.btn1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.Btn1_Click);
+            // 
+            // listB1
+            // 
+            this.listB1.FormattingEnabled = true;
+            this.listB1.Location = new System.Drawing.Point(468, 27);
+            this.listB1.Name = "listB1";
+            this.listB1.Size = new System.Drawing.Size(290, 355);
+            this.listB1.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(286, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Palavra";
+            // 
+            // txb3
+            // 
+            this.txb3.Location = new System.Drawing.Point(220, 190);
+            this.txb3.Name = "txb3";
+            this.txb3.Size = new System.Drawing.Size(186, 20);
+            this.txb3.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txb3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listB1);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txb2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txb1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ckb2);
+            this.Controls.Add(this.ckb1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Arranjo Fatal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox ckb1;
+        private System.Windows.Forms.CheckBox ckb2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox txb1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txb2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.ListBox listB1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txb3;
+    }
+}
+
---
+++ ProbOne/ProbOne1/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProbOne1
+{
+    public partial class Form1 : Form
+    {
+        int var1 = 0;
+        int var2 = 0;
+        int var3 = 0;
+        int var4 = 0;
+        int p = 0;
+        int n = 0;
+        int z = 0;
+        int arranjo = 0;
+        int fatorial = 0;
+
+        
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        public void Clear()
+        {
+            int var1 = 0;
+            int var2 = 0;
+            int p = 0;
+            int n = 0;
+            int z = 0;
+            int arranjo = 0;
+            int fatorial = 0;
+
+            ckb1.Checked = false;
+            ckb2.Checked = false;
+            txb1.Text=("");
+            txb2.Text=("");
+            txb3.Text=("");
+            listB1.Text=("");
+            progressBar1.Value = 0;
+        }
+
+ 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Por Favor, " + "\n" + "Escolha uma opção");
+
+            txb1.Enabled = false;
+            txb2.Enabled = false;
+            txb3.Enabled = false;
+            btn1.Enabled = false;
+        }
+
+        private void Ckb1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ckb1.Checked == true)
+            {
+                ckb2.Enabled = false;
+                txb1.Enabled = true;
+                txb2.Enabled = true;
+                txb3.Enabled = false;
+            }
+            else
+            {
+                MessageBox.Show("Selecione uma opção!");
+
+                ckb2.Enabled = true;
+                txb1.Enabled = false;
+                txb2.Enabled = false;
+                txb3.Enabled = false;
+            }
+        }
+
+        private void Ckb2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ckb2.Checked == true)
+            {
+                ckb1.Enabled = false;
+                txb3.Enabled = true;
+                txb1.Enabled = false;
+                txb2.Enabled = false;
+
+            }
+            else
+            {
+                MessageBox.Show("Selecione uma opção!");
+
+                ckb1.Enabled = true;
+                txb1.Enabled = false;
+                txb2.Enabled = false;
+                txb3.Enabled = false;
+            }
+        }
+
+        private void Txb1_TextChanged_1(object sender, EventArgs e)
+        {
+            
+            var1 = Convert.ToInt32(txb1.Text);
+
+            if (var1 != 0)
+            {
+                if (var2 != 0)
+                {
+                    btn1.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("Favor digitar um valor no campo: 'Formato do Agrupamento'");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Favor digite algum valor");
+            }
+        }
+
+        private void Txb2_TextChanged_1(object sender, EventArgs e)
+        {
+            var2 = Convert.ToInt32(txb2.Text);
+
+            if(var2 != 0)
+            {
+                if (var1 != 0)
+                {
+                    btn1.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("Favor digitar um valor no campo: 'Número de Elementos'");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Favor digite algum valor");
+            }
+        }
+
+
+
+        private void ProgressBar1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Maximum = 20;
+        }
+
+        public void Progresso()
+        {
+            while (z <= 100)
+            {
+                if (z == 100)
+                {                    
+                    break;                    
+                }
+                else
+                {
+                    progressBar1.Value += 1;
+                    z++;
+                }
+                
+            }
+
+        }
+        
+        private void Btn1_Click(object sender, EventArgs e)
+        {
+            Progresso();
+
+            //var3 = Convert.ToInt32(txb1.Text);
+
+            //var4 = Convert.ToInt32(txb2.Text);
+
+            for (int i = 1; i <= (2 - var1); i++)
+            {
+                listB1.Items.Add(Convert.ToString(i + " fatorial= " + fatorial));
+                arranjo = i;
+                fatorial = arranjo * i;                
+            }
+
+            p = (var1 - var2);
+            n = (arranjo);
+
+            MessageBox.Show("Resposta: " + n);
+
+            Clear();
+        }
+
+        
+    }
+}
